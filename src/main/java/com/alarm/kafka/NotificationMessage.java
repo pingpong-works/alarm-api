df0d37b2424d93da44cc146f6a05f1d1f477573a
quokkavely <yellowjerri@gmail.com>
2024-10-07 11:22:49 +0900

    fix/bug(core-alarm)-trust-package(#9)

--- src/main/java/com/alarm/kafka/NotificationMessage.java
+++ src/main/java/com/alarm/kafka/NotificationMessage.java
@@ -1,19 +1,23 @@
-package com.alarm.notification;
+package com.alarm.kafka;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class NotificationMessage {
     private Long employeeId;
     private String message;
+    private Long typeId;
     private NotificationType type;
 
     public enum NotificationType {
         APPROVAL,
         NOTICE,
         CHAT,
-        EMAIL;
+        EMAIL,
+        DOCUMENT;
     }
 }
